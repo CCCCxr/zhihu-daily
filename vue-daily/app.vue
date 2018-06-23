fc9675892e1907332c2806b126fc9dbec4698d0f
Chenxr <402367310@qq.com>
2018-06-24 03:46:05 +0800

    web版本

---
+++ vue-daily/app.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="daily">
+    <div class="daily-menu">
+      <div class="daily-menu-item" 
+          :class="{on: type === 'recommend'}" 
+          @click="handleToRecommend">每日推荐</div>
+      <div class="daily-menu-item" 
+          :class="{on: type === 'daily'}"
+          @click="showThemes = !showThemes" >主题日报</div>
+      <ul v-show="showThemes">
+        <li v-for="item in themes" :key="item.id">
+          <a :class="{on: item.id === themeId && type === 'daily'}"
+              @click="handleToTheme(item.id)">{{item.name}}</a>
+        </li>
+      </ul>
+    </div>
+    <div class="daily-list" ref="list">
+      <template v-if="type === 'recommend'">
+        <div v-for='list in recommendList' :key='list.date'>
+          <div class="daily-date">{{ formatDay(list.date) }}</div>
+          <Item
+            v-for="item in list.stories"
+            :data="item"
+            :key="item.id"
+            @click.native="handleClick(item.id)"></Item>
+        </div>
+      </template>
+      <template v-if="type === 'daily'">
+        <Item
+          v-for="item in list"
+          :data="item"
+          :key="item.id"
+          @click.native="handleClick(item.id)"></Item>
+      </template>
+    </div>
+    <daily-article :id="articleId"></daily-article>
+  </div>
+</template>
+<script>
+import $ from './libs/util';
+import Item from './components/item.vue';
+import dailyArticle from './components/daily-article.vue';
+export default {
+  components: { Item, dailyArticle },
+  data(){
+    return {
+      themes:[],
+      showThemes: false,
+      type: 'recommend',
+      themeId: 0,
+      recommendList: [],
+      list:[],
+      dailyTime: $.getTodayTime(),
+      isLoading: false,
+      articleId: 0
+    }
+  },
+  methods: {
+    getThemes () {
+      // axios 发起get请求
+      $.ajax.get('themes').then(res => {
+        this.themes = res.others;
+      })
+    },
+    handleToTheme (id) {
+      // 改变菜单分类
+      this.type = 'daily';
+      // 设置当前点击子类的主题日报Id
+      this.themeId = id;
+      // 清空中间栏数据
+      this.list = [];
+      $.ajax.get('theme/' + id).then( res => {
+        // 过滤掉类型为1的文章，该类型文章为空
+        this.list = res.stories.filter( item => item.type !== 1);
+      })
+    },
+    handleToRecommend () {
+      this.type = 'recommend';
+      this.recommendList = [];
+      this.dailyTime = $.getTodayTime();
+      this.getRecommendList();
+    },
+    getRecommendList () {
+      this.isLoading = true;
+      const prevDay = $.prevDay(this.dailyTime );
+      $.ajax.get('news/before/' + prevDay).then(res => {
+        this.recommendList.push(res);
+        this.isLoading = false;
+      })
+    },
+    // 时间格式转换
+    formatDay (date) {
+      let month = date.substr(4,2);
+      let day = date.substr(6,2);
+      if (month.substr(0,1) === "0") month = month.substr(1,1);
+      if (day.substr(0,1) === '0') day = day.substr(1,1);
+      return `${month} 月 ${day} 日`;
+    },
+    handleClick (id) {
+      this.articleId = id;
+    }
+  },
+  mounted() {
+    // 初始化调用
+    this.getThemes();
+    this.getRecommendList();
+    //获取DOM
+    const $list = this.$refs.list;
+    // 监听中栏的滚动事件
+    $list.addEventListener('scroll', () => {
+      // 在'主题日报'或加载推荐列表时停止操作
+      if (this.type === 'daily' || this.isLoading) return ;
+      if ($list.scrollTop + document.body.clientHeight >= $list.scrollHeight) {
+        this.dailyTime -= 86400000;
+        this.getRecommendList();
+      }
+    });
+  }
+}
+</script>
+
